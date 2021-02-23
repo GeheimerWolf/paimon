1aa96dfa5930a48f1a85462aa0b0f7bea8ef0378
GeheimerWolf <PatrickBruederle@gmail.com>
2021-02-23 16:38:24 +0100

    deps.ts

--- deps.ts
+++ deps.ts
@@ -1,8 +1,6 @@
 export * from "https://deno.land/std@0.85.0/fmt/colors.ts";
 export { fromFileUrl, toFileUrl } from "https://deno.land/std@0.85.0/path/mod.ts";
 export * as confusables from "https://raw.githubusercontent.com/discordeno/discordeno/master/mod.ts";
-export * from "https://raw.githubusercontent.com/discordeno/discordeno/master/mod.ts";
-export { Image } from "https://raw.githubusercontent.com/matmen/ImageScript/deno/mod.ts";
-export * from "https://raw.githubusercontent.com/Skillz4Killz/Sabr/master/mod.ts";
+export * from "https://deno.land/x/discordeno@10.2.0/mod.ts";
+export * from "https://deno.land/x/sabr@1.1.2/mod.ts";
 export * from "./cache.ts";
-console.log("deps.ts exported everything")
